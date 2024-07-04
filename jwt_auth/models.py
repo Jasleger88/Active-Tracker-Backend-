9af976fed4e1291d447c06e1e2f1b2abda092cda
Jasmine Leger <Jasmineleger21@gmail.com>
2024-07-04 11:01:11 -0400

    inital commit

---
+++ jwt_auth/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django .contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+
+
+class User(AbstractUser):
+    email =models.CharField(max_length =50, unique=True)
+    first_name = models.CharField(max_length =50)
+    last_name = models.CharField(max_length =50)
+    profile_image = models.CharField(max_length=350) 
+
---
+++ jwt_auth/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model, password_validation
+from django.contrib.auth.hashers import make_password
+from django.core.exceptions import ValidationError
+
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+    password_confirmation = serializers.CharField(write_only=True)
+
+    def validate(self, data): 
+        print('DATA',data)
+        password = data.pop('password')
+        password_confirmation = data.pop('password_confirmation')
+
+        if password != password_confirmation:
+            raise ValidationError({'password_confirmation': 'passwords do not match'})
+        
+        try:
+            password_validation.validate_password(password=password)
+        except ValidationError as err:
+            print('VALIDATION ERROR')
+            raise ValidationError({'password': err.messages})
+
+
+#HASH THE PASSWORD
+        data['password'] = make_password(password)
+
+        print('DATA ->', data)
+       
+        return data
+
+
+    class Meta:
+        model = User
+        fields = ('id','username', 'first_name', 'last_name', 'profile_image', 'email', 'password', 'password_confirmation')

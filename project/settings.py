ca822e62ac38481fa8ff48e60531f438ac653072
Jasmine Leger <Jasmineleger21@gmail.com>
2024-07-12 06:33:13 -0400

    ready to deploy

--- project/settings.py
+++ project/settings.py
@@ -9,8 +9,15 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+import django_on_heroku
+from dotenv import load_dotenv
+import dj_database_url
+load_dotenv()
+
+
+ENV = str(os.getenv('ENVIRONMENT', 'DEV'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +27,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4r(k313+jg0ze-%79%ghfw-#1z)dn=s&=0=h@@w&)#8))nsfgd'
+if ENV === 'DEV'
+    SECRET_KEY = 'django-insecure-4r(k313+jg0ze-%79%ghfw-#1z)dn=s&=0=h@@w&)#8))nsfgd'
+
+else:
+    SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = ENV == 'DEV'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -86,13 +97,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+DATABASES = {}
+if ENV != 'DEV':
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)	
+else:
+    Datebases ['default'] = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'active-tracker-api',
         'HOST': 'localhost',
         'PORT': 5432
-    }
 }
 
 
@@ -147,3 +160,5 @@ REST_FRAMEWORK = {
     'jwt_auth.authentication.JWTAuthentication'
     ],
 }
+
+django_on_heroku.settings(locals())

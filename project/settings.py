9c2762d08bdc3bf22bfb07aae88663472c85e3f8
Jasmine Leger <Jasmineleger21@gmail.com>
2024-07-12 06:39:49 -0400

    ready to deploy

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if ENV == 'DEV'
+if ENV == 'DEV':
     SECRET_KEY = 'django-insecure-4r(k313+jg0ze-%79%ghfw-#1z)dn=s&=0=h@@w&)#8))nsfgd'
 
 else:
